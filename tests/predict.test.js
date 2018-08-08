ff87523fb0fa9b8841ba2f3ff418dd74dfd63028
Lian <draggili@gmail.com>
2018-08-08 17:17:47 +0200

    setup tests

---
+++ tests/predict.test.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const predict = require('../index').predict;
+
+chai.use(sinonChai);
+const expect = chai.expect;
+
+describe('The Predict function', () => {
+  it('should be triggered', () => {
+    const name = 'Foobar';
+    const req = {
+      body: {
+        name: name
+      }
+    };
+    const res = { send: sinon.stub() };
+
+    // Call tested function
+    predict(req, res);
+
+    // Verify behavior of tested function
+    expect(res.send.calledOnce).to.be.true;
+    expect(res.send.firstCall.args).to.deep.equal([`Hello ${name}!`]);
+  })
+});
