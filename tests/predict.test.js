453b77b90a85e15fe8bf212a93f2e6fa722760c4
Lian <draggili@gmail.com>
2018-08-08 17:57:27 +0200

    move functions

--- tests/predict.test.js
+++ tests/predict.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
-const predict = require('../index').predict;
+const predict = require('../functions/index').predict;
 
 chai.use(sinonChai);
 const expect = chai.expect;
